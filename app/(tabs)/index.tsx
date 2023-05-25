5acf52180a39a2b67319a50ffea589f9f0ba9365
Jeff Boek <jeff@jeffboek.com>
2023-05-24 17:34:07 -0700

    not sur but it work

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-red-500">
       <Text style={styles.title}>Hello Dan</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
